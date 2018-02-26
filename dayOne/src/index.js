d306211f78ef3e22f65824de0b4d122f411a5624
yzdd <yjs10086>
2018-02-26 14:54:21 +0800

    将图片转化为 base64格式的 file-loader已经过期，这个转化出来返回的是文件名,url-loader返回的则是base64格式文件

--- dayOne/src/index.js
+++ dayOne/src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash'
 import "./common/style.css";
+import header from "./common/img/header.jpg";
 
 function component() {
   var divDom = document.createElement("div");
   divDom.innerHTML = ['hello', 'webpack'].join("-");
+  var imgDom = new Image();
+  imgDom.src = header;
+
   var pDom = document.createElement("p");
   pDom.innerText = _.join(['hello', 'world'], "=");
+
   divDom.appendChild(pDom);
+  divDom.appendChild(imgDom);
   return divDom;
 }
 
--- dayOne/webpack.config.js
+++ dayOne/webpack.config.js
@@ -1,21 +1,18 @@
 const path = require("path");
 
 
-
-console.log(path.resolve(__dirname , 'dist'),"_--当前路径");
+console.log(path.resolve(__dirname, 'dist'), "_--当前路径");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname , 'dist')
-	},
-	module: {
-		rules:[
-			{
-				test:/\.css$/,
-				use: ['style-loader','css-loader']
-			}
-		]
-	}
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.(png|jpg|jpeg|gif)$/, use: ['url-loader']}
+    ]
+  }
 };
