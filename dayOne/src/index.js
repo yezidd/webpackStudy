277b563b42f982c27453db9e28f7f1638ef0688f
yzdd <yjs10086>
2018-02-26 14:12:29 +0800

    css的打包 需要先 通过 style-loader然后再经过css-loader

--- dayOne/src/index.js
+++ dayOne/src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash'
+import "./common/style.css";
 
-function component(){
-	var divDom  = document.createElement("div");
-	divDom.innerHTML = ['hello','webpack'].join("-");
-	var pDom = document.createElement("p");
-	pDom.innerText = _.join(['hello','world'],"=");
-	divDom.appendChild(pDom);
-	return divDom;
+function component() {
+  var divDom = document.createElement("div");
+  divDom.innerHTML = ['hello', 'webpack'].join("-");
+  var pDom = document.createElement("p");
+  pDom.innerText = _.join(['hello', 'world'], "=");
+  divDom.appendChild(pDom);
+  return divDom;
 }
+
 document.body.appendChild(component());
--- dayOne/webpack.config.js
+++ dayOne/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+
+
 console.log(path.resolve(__dirname , 'dist'),"_--当前路径");
 
 module.exports = {
@@ -8,7 +10,7 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname , 'dist')
 	},
-	modules: {
+	module: {
 		rules:[
 			{
 				test:/\.css$/,
